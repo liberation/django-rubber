732fbf83170477d2b4bae4db3d2ee335a42e4c93
Laurent Guilbert <laurent.guilbert91@gmail.com>
2017-05-22 16:37:02 +0200

    Handle iterator in tqdm.

--- rubber/management/commands/es_create_documents.py
+++ rubber/management/commands/es_create_documents.py
@@ -12,11 +12,9 @@ class Command(ESBaseCommand):
         for model in self.rubber_config.indexable_models:
             self.print_info(u"Indexing model: '{0}'.".format(model.__name__))
             queryset = model.get_indexable_queryset()
-            progress = tqdm(total=queryset.count(), dynamic_ncols=True)
-            for obj in queryset:
+            for obj in tqdm(queryset):
                 if not self.dry_run:
                     try:
                         obj.es_index(async=False)
                     except Exception as exc:
                         print exc
-                progress.update()
